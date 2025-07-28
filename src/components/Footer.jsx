d96db41842594ef0a490c2dae1ac3e9ee0865a24
nyahalo <christiansammanuel@gmail.com>
2025-07-28 20:54:25 +0700

    footer done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import { FaFacebookF, FaInstagram, FaTwitter } from 'react-icons/fa'; // Anda perlu menginstal react-icons
+import { FaFacebookF, FaInstagram, FaTiktok, FaTwitter, FaWhatsapp } from 'react-icons/fa'; // Anda perlu menginstal react-icons
 
 export default function Footer() {
   return (
@@ -12,15 +12,21 @@ export default function Footer() {
           <p className="mt-2 text-sm">&copy; {new Date().getFullYear()} OCEANTIC. All rights reserved.</p>
         </div>
         <div className="flex space-x-6">
-          <a href="#" target="_blank" rel="noopener noreferrer" className="text-white hover:text-aqua-accent transition duration-300 text-2xl">
+          <a href="https://www.facebook.com/people/Oceantic-Official/61578010997154/" target="_blank" rel="noopener noreferrer" className="text-white hover:text-aqua-accent transition duration-300 text-2xl">
             <FaFacebookF />
           </a>
-          <a href="#" target="_blank" rel="noopener noreferrer" className="text-white hover:text-aqua-accent transition duration-300 text-2xl">
+          <a href="https://www.instagram.com/oceantic_official" target="_blank" rel="noopener noreferrer" className="text-white hover:text-aqua-accent transition duration-300 text-2xl">
             <FaInstagram />
           </a>
-          <a href="#" target="_blank" rel="noopener noreferrer" className="text-white hover:text-aqua-accent transition duration-300 text-2xl">
-            <FaTwitter />
+
+          <a href="https://api.whatsapp.com/send/?phone=62895365089206&text&type=phone_number&app_absent=0" target="_blank" rel="noopener noreferrer" className="text-white hover:text-aqua-accent transition duration-300 text-2xl">
+            <FaWhatsapp />
           </a>
+          <a href="https://www.tiktok.com/@oceanticsports" target="_blank" rel="noopener noreferrer" className="text-white hover:text-aqua-accent transition duration-300 text-2xl">
+            <FaTiktok />
+          </a>
+
+
         </div>
       </div>
     </footer>
